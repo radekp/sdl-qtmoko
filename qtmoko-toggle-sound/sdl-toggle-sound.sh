19d60461486dfd3e3aa14482ae7c046fb544e2ac
Radek Polak <psonek2@seznam.cz>
2013-01-12 21:36:05 +0100

    sdl-toggle-sound.sh - use qui for the message box

--- qtmoko-toggle-sound/sdl-toggle-sound.sh
+++ qtmoko-toggle-sound/sdl-toggle-sound.sh
@@ -2,34 +2,8 @@
 if [ -f ~/.sdlnosound ]
 then
 	rm -f ~/.sdlnosound
-	echo '\n\n\nSound in SDL is now\n\n\n           E N A B L E D\n\n\n' > /tmp/banner.txt
-	echo '            _ ' >> /tmp/banner.txt
-	echo '           / |' >> /tmp/banner.txt
-	echo '          /  |' >> /tmp/banner.txt
-	echo '      ___/   |' >> /tmp/banner.txt
-	echo '     /   |   |' >> /tmp/banner.txt
-	echo '     |   |   |' >> /tmp/banner.txt
-	echo '     |   |   |' >> /tmp/banner.txt
-	echo '     \___|   |' >> /tmp/banner.txt
-	echo '         \\   |' >> /tmp/banner.txt
-	echo '          \\  |' >> /tmp/banner.txt
-	echo '           \\_|' >> /tmp/banner.txt
+	qui showmessage SDL "Sound in SDL is now ON"
 else
 	touch ~/.sdlnosound
-	echo '\n\n\nSound in SDL is now\n\n\n           D I S A B L E D\n\n\n' > /tmp/banner.txt
-	echo '       _    _ ' >> /tmp/banner.txt
-	echo '       \\\\  / |' >> /tmp/banner.txt
-	echo '        \\\\/  |' >> /tmp/banner.txt
-	echo '      ___\\\\  |' >> /tmp/banner.txt
-	echo '     /   |\\\\ |' >> /tmp/banner.txt
-	echo '     |   | \\\\|' >> /tmp/banner.txt
-	echo '     |   |  \\\\' >> /tmp/banner.txt
-	echo '     \___|   \\\\' >> /tmp/banner.txt
-	echo '         \   |\\\\' >> /tmp/banner.txt
-	echo '          \  | \\\\' >> /tmp/banner.txt
-	echo '           \_|  \\\\' >> /tmp/banner.txt
+	qui showmessage SDL "Sound in SDL is now OFF"
 fi
-
-qterminal -c cat /tmp/banner.txt
-
-sleep 1
